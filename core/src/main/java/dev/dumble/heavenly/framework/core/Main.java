672aa1bf0f48079294c01a62451de516d9777445
Lecer <xarshiapro@gmail.com>
2023-09-05 18:12:32 +0330

    added shadowJar & cleaned up modules

---
+++ core/src/main/java/dev/dumble/heavenly/framework/core/Main.java
@@ -0,0 +1,7 @@
+package dev.dumble.heavenly.framework.core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/dev/dumble/heavenly/framework/Main.java
+++
@@ -1,8 +0,0 @@
-package dev.dumble.heavenly.framework;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Minecraft!");
-    }
-}
